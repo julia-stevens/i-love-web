c9583282156bf615a616d812b6a5f8e4e7380dce
julia-stevens <juliastevens02@gmail.com>
2025-04-06 13:05:09 +0200

    md bestanden inladen

--- server.js
+++ server.js
@@ -1,8 +1,37 @@
 import express from "express"
-const app = express()
+import { Liquid } from "liquidjs"
+import { readdir, readFile } from "node:fs/promises"
 
+const files = await readdir("content") // tot nu toe fetch naar Directus, nu doen we dit lokaal
+
+console.log(files)
+
+const app = express()
 app.use(express.static("public"))
+
+const engine = new Liquid()
+app.engine("liquid", engine.express())
+
+app.set("views","./views")
+
 app.use(express.urlencoded({ extended: true }))
+
+
+app.get("/", async function (request, response) {
+    response.render("home.liquid", {
+        files: files
+    })
+}) 
+
+app.get("/:slug", async function (request, response) {
+    console.log(request.params)
+
+    const fileContents = await readFile("content/" + request.params.slug + ".md", { encoding: "utf8" })
+
+    response.render("artikel.liquid", {
+        fileContents: fileContents
+    })
+})
 
 app.set('port', process.env.PORT || 8000)
 
