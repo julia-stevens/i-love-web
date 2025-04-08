4cd0e19376c5eae99bcefa65bb1f58059bf6ba6f
julia-stevens <juliastevens02@gmail.com>
2025-04-08 10:23:45 +0200

    Markdown parsen en omzetten naar HTML

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 import express from "express"
 import { Liquid } from "liquidjs"
 import { readdir, readFile } from "node:fs/promises"
+import { marked } from "marked"
 
 const files = await readdir("content") // tot nu toe fetch naar Directus, nu doen we dit lokaal
-
-console.log(files)
 
 const app = express()
 app.use(express.static("public"))
@@ -24,12 +23,17 @@ app.get("/", async function (request, response) {
 }) 
 
 app.get("/:slug", async function (request, response) {
-    console.log(request.params)
+    // console.log(request.params)
 
     const fileContents = await readFile("content/" + request.params.slug + ".md", { encoding: "utf8" })
+    // const markedUp = marked.parse("content/" +  + request.params.slug)
+    const markedUp = marked.parse(fileContents)
 
+    // console.log(markedUp)
+    
     response.render("artikel.liquid", {
-        fileContents: fileContents
+        fileContents: fileContents,
+        markedUp: markedUp
     })
 })
 
